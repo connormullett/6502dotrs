50cc50b6a598c1f35c188a0a4531e43aa78401ce
connormullett <connormullett@gmail.com>
2022-12-10 16:47:23 -0500

    fetch instructions from memory

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,7 @@
-use crate::{memory::Memory, processor_status::ProcessorStatus};
+use crate::{
+    memory::{self, Memory},
+    processor_status::ProcessorStatus,
+};
 
 #[derive(Debug, Default, Clone)]
 pub struct Cpu {
@@ -33,5 +36,21 @@ impl Cpu {
         self.ps.clear();
 
         self.to_owned()
+    }
+
+    pub fn execute(&mut self) {
+        loop {
+            let instruction = self.fetch();
+        }
+    }
+
+    fn fetch(&mut self) -> u8 {
+        if self.pc as usize > memory::MAX_MEM {
+            panic!("PC exceeds max memory allocated {}", memory::MAX_MEM);
+        }
+
+        let data = self.memory.data[self.pc as usize];
+        self.pc += 1;
+        data
     }
 }
